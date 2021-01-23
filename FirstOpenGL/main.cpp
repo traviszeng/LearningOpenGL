0c0ae236520444d23972f03a1a83f6b0dad5d21f
traviszeng <traviszeng7@gmail.com>
2021-01-23 12:10:04 +0800

    Initial Commit

---
+++ FirstOpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FirstOpenGL
+//
+//  Created by 曾健 on 2021/1/23.
+//  Copyright © 2021 traviszeng.io. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
